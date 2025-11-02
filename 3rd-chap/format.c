cf885f0efeed92cbb767dca071faeecb2f2d4d9c
Aman-Kasa <a.kasa@alustudent.com>
2025-11-02 02:35:23 +0200

    3rd chap mini

---
+++ 3rd-chap/format.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+	int main(void){
+		int x, y;
+		float z, q;
+
+		x = 10;
+		y = 20;
+		z = 10.05;
+		q = 20.05;
+
+		printf("x = %d, y = %d, z = %f, q = %f\n", y, x, z, q);
+
+	return 0;
+
+	}
---
+++ 3rd-chap/wrong.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+	int main(void){
+
+		int x = 5;
+
+		printf("x = %d\n", x);   // add double %d%d or another variable after x like y... aka wrong compilation
+
+	return 0;
+
+	}
